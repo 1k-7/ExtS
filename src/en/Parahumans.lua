726430496a4f861b6b4b04d1a58cee883ffdff33
JFronny <git@akbvopenfl1.hopto.org>
2024-09-21 21:56:54 +0200

    add Parahumans (+ Wildbows other works)

---
+++ src/en/Parahumans.lua
@@ -0,0 +1,149 @@
+-- {"id":-2129381796,"ver":"1.0.0","libVer":"1.0.0","author":"JFronny"}
+
+local settings = {}
+
+-- concatenate two lists
+---@param list1 table
+---@param list2 table
+local function concatLists(list1, list2)
+    for i = 1, #list2 do
+        table.insert(list1, list2[i])
+    end
+    return list1
+end
+
+local function lift(xtable)
+    local t = {}
+    for k, v in next, xtable do
+        table.insert(t, { k, v })
+    end
+    return t
+end
+
+local auxiliary = {
+    ["https://parahumans.wordpress.com/"] = {
+        name = "Worm",
+        image = "https://parahumans.wordpress.com/wp-content/uploads/2011/06/cityscape2.jpg",
+        description = "#content .entry-content p:nth-child(n+5):nth-child(-n+9)",
+        arcs = ".widget_categories .cat-item .cat-item:not(.cat-item .cat-item .cat-item)",
+        arc = "a",
+        chapters = ".cat-item .cat-item a"
+    },
+    ["https://pactwebserial.wordpress.com/"] = {
+        name = "Pact",
+        image = "https://pactwebserial.wordpress.com/wp-content/uploads/2014/01/pact-banner2.jpg",
+        description = "#content .entry-content p:nth-child(n+2):nth-child(-n+5)",
+        arcs = ".widget_categories .cat-item .cat-item:not(.cat-item .cat-item .cat-item)",
+        arc = "a",
+        chapters = ".cat-item .cat-item a"
+    },
+    -- We ignore Twig since that uses an abysmal table of contents.
+    -- If Wildbow though anyone might want to read it, he would have made it easier to read.
+    --["https://twigserial.wordpress.com/"] = {
+    --    name = "Twig",
+    --    image = "https://twigserial.wordpress.com/wp-content/uploads/2016/06/cropped-twigheader5.png",
+    --    description = "#content .entry-content p:nth-child(n+2):nth-child(-n+3)",
+    --},
+    ["https://www.parahumans.net/"] = {
+        name = "Ward",
+        image = "https://i2.wp.com/www.parahumans.net/wp-content/uploads/2017/10/cropped-Ward-Banner-Proper-1.jpg",
+        description = "#content .entry-content p:nth-child(n+5):nth-child(-n+6)",
+        arcs = "#secondary .widget_nav_menu:not(#nav_menu-2) .menu-item:not(.menu-item .menu-item)",
+        arc = "a",
+        chapters = ".menu-item .menu-item a"
+    },
+    ["https://palewebserial.wordpress.com/"] = {
+        name = "Pale",
+        image = "",
+        description = "#content .entry-content p:nth-child(n+1):nth-child(-n+7)",
+        arcs = "#nav_menu-2 .menu-item:not(.menu-item .menu-item)",
+        arc = "a",
+        chapters = ".sub-menu a"
+    }
+}
+
+local function parseNovel(novelURL, loadChapters)
+    local document = GETDocument(novelURL)
+    local aux = auxiliary[novelURL]
+
+    local novel = NovelInfo {
+        title = aux.name,
+        imageURL = aux.image,
+        description = document:select(aux.description):text(),
+        authors = { "Wildbow" },
+        status = NovelStatus.COMPLETED
+    }
+
+    if loadChapters then
+        local chaps = {}
+        local i = 0
+        map(document:select(aux.arcs), function(element)
+            local arcPrefix = element:selectFirst(aux.arc):text()
+                    :gsub("^[ ]*", "")
+                    :gsub("[ ]*$", "")
+                    :gsub("^Arc [0-9]* ", "")
+                    :gsub("– ", "")
+                    :gsub("^%(", "")
+                    :gsub("%)$", "")
+                    .. " - "
+            chaps = concatLists(chaps, mapNotNil(element:select(aux.chapters), function(v)
+                i = i + 1
+                return NovelChapter {
+                    order = i,
+                    title = arcPrefix .. v:text(),
+                    link = v:attr("href")
+                }
+            end))
+        end)
+
+        novel:setChapters(AsList(chaps))
+    end
+
+    return novel
+end
+
+--- Get a chapter passage based on its chapterURL.
+---
+--- Required.
+---
+--- @param chapterURL string The chapters shrunken URL.
+--- @return string Strings in lua are byte arrays. If you are not outputting strings/html you can return a binary stream.
+local function getPassage(chapterURL)
+    local document = GETDocument(chapterURL)
+    map(document:select(".entry-content > :not(p, h1, hr)"), function(v) v:remove() end)
+    return pageOfElem(document:selectFirst(".entry-content"), true)
+end
+
+return {
+    -- Required
+    id = -2129381796,
+    name = "Wildbow (Parahumans)",
+    baseURL = "https://www.parahumans.net/",
+    listings = {
+        Listing("Novels", false, function(data)
+            return map(lift(auxiliary), function(a)
+                return Novel {
+                    link = a[1],
+                    title = a[2].name,
+                    imageURL = a[2].image
+                }
+            end)
+        end)
+    },
+    getPassage = getPassage,
+    parseNovel = parseNovel,
+    shrinkURL = function(url) return url end,
+    expandURL = function(url) return url end,
+
+    -- Optional values to change
+    imageURL = "https://parahumans.wordpress.com/wp-content/uploads/2011/06/cityscape2.jpg",
+    hasCloudFlare = false,
+    hasSearch = false,
+    chapterType = ChapterType.HTML,
+    startIndex = 1,
+
+    -- Required if [settings] is not empty
+    updateSetting = function(id, value)
+        settings[id] = value
+    end,
+}
